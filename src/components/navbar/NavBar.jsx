92d9dc33dc094d1c20eb98fb8e7150884ac8813c
ugolbck <ugo813@hotmail.fr>
2023-07-20 09:12:39 +0200

    started navbar glassmorphism onscroll

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
+    const [scroll, setScroll] = useState(false);
+    useEffect(() => {
+    window.addEventListener("scroll", () => {
+        setScroll(window.scrollY > 50);
+    });
+    }, []);
+
     return (
-        <div className="nav-bar">
+        <div className={`nav-bar ${scroll ? "nav-bar-border": ""}`}>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
